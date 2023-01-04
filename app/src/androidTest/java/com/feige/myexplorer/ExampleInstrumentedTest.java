952dde56d7ee14b2f4547cac5c0fb601b1433219
wutengfei <781862614@qq.com>
2023-01-04 18:00:53 +0800

    [update]修改包名

--- app/src/androidTest/java/com/feige/myexplorer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/feige/myexplorer/ExampleInstrumentedTest.java
@@ -1,14 +1,14 @@
-package com.example.myapplication;
+package com.feige.myexplorer;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+        assertEquals("com.feige.myexplorer", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/feige/myexplorer/MainActivity.java
+++ app/src/main/java/com/feige/myexplorer/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.feige.myexplorer;
 
 import android.Manifest;
 import android.app.Activity;
