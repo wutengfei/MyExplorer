4900aee5ef5d966fd14cb682850cd5c1eb7b15bb
wutengfei <781862614@qq.com>
2023-01-04 17:55:06 +0800

    [update]添加扫码功能

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -28,30 +32,41 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import java.util.Objects;
+
+import cn.org.bjca.signet.component.qr.activity.SignetQrApi;
+import cn.org.bjca.signet.component.qr.bean.QrResultBean;
+import cn.org.bjca.signet.component.qr.callback.QrBaseCallBack;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText et_url;
     private Button btn_enter;
     private WebView webview;
+    private ImageView qr_scan;
     private boolean mIsExit;
     private FrameLayout mLayout;
     private LinearLayout ll_title;
     private boolean isLandscape;
+    private Context context;
+    private static final int QRSCAN_REQ_CAMERA_PERMISSION = 776;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = MainActivity.this;
         Objects.requireNonNull(getSupportActionBar()).hide();
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -59,12 +74,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         et_url = findViewById(R.id.et_url);
         btn_enter = findViewById(R.id.btn_enter);
+        qr_scan = findViewById(R.id.qr_scan);
         webview = findViewById(R.id.webview);
         mLayout = findViewById(R.id.fl_video);
         ll_title = findViewById(R.id.ll_title);
-        initWebView(webview);
         btn_enter.setOnClickListener(this);
-
+        qr_scan.setOnClickListener(this);
+        initWebView(webview);
         //在该Editview获得焦点的时候将“回车”键改为“搜索”
         et_url.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
         et_url.setInputType(EditorInfo.TYPE_CLASS_TEXT);
@@ -84,14 +100,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.btn_enter) {
             search(view);
+        } else if (view.getId() == R.id.qr_scan) {
+            if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) && (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED)) {
+                ((Activity) context).requestPermissions(new String[]{Manifest.permission.CAMERA}, QRSCAN_REQ_CAMERA_PERMISSION);
+            } else {
+                qrScan();
+            }
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void search(View view) {
+        webview.removeAllViews();
         String url = et_url.getText().toString().trim();
         if (!TextUtils.isEmpty(url)) {
             if (url.startsWith("https") || url.startsWith("http")) {
@@ -112,32 +138,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
+    private void qrScan() {
+        SignetQrApi.useQrFunc(new QrBaseCallBack(context) {
+            @Override
+            public void onQrResult(final QrResultBean result) {
+                if (result.getQrCode() == null) {
+                    Toast.makeText(context, "扫描结果为空", Toast.LENGTH_SHORT).show();
+                } else {
+                    String str = result.getQrCode();
+                    if (webview != null) {
+                        boolean isMatchesUrl = Patterns.WEB_URL.matcher(str).matches();
+                        if (isMatchesUrl) {
+                            webview.loadUrl(str);
+                        } else {
+                            TextView textView = new TextView(context);
+                            textView.setPadding(50, 30, 50, 30);
+                            textView.setTextIsSelectable(true);
+                            textView.setText(str);
+                            textView.setTextColor(Color.BLACK);
+
+                            webview.removeAllViews();
+                            webview.clearHistory();
+                            webview.clearView();
+                            webview.loadUrl("");
+                            webview.addView(textView);
+                        }
+                    } else {
+                        Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+    }
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void initWebView(final WebView webView) {
 
+        webView.getSettings().setDatabaseEnabled(true);
         webView.getSettings().setAllowFileAccess(true);//设置在WebView内部是否允许访问文件，默认允许访问。
-        webView.getSettings().setAllowFileAccessFromFileURLs(false);//设置WebView运行中的一个文件方案被允许访问其他文件方案中的内容，默认值true
-        webView.getSettings().setAllowContentAccess(false);//设置WebView是否使用其内置的变焦机制，该机制结合屏幕缩放控件使用，默认是false，不使用内置变焦机制
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);//设置WebView运行中的一个文件方案被允许访问其他文件方案中的内容，默认值true
+        webView.getSettings().setAllowContentAccess(true);//设置WebView是否使用其内置的变焦机制，该机制结合屏幕缩放控件使用，默认是false，不使用内置变焦机制
         webView.getSettings().setAllowUniversalAccessFromFileURLs(true);//设置WebView运行中的脚本可以是否访问任何原始起点内容，默认true
         webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(false);//设置是否开启DOM存储API权限，默认false，未开启，设置为true，WebView能够使用DOM storage API
-        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setDomStorageEnabled(true);//设置是否开启DOM存储API权限，默认false，未开启，设置为true，WebView能够使用DOM storage API
         webView.getSettings().setLoadsImagesAutomatically(true);//设置WebView是否加载图片资源，默认true，自动加载图片
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);//设置脚本是否允许自动打开弹窗，默认false，不允许
 
-        webView.setVerticalScrollBarEnabled(false);// 取消Vertical ScrollBar显示
-        webView.setHorizontalScrollBarEnabled(false);// 取消Horizontal ScrollBar显示
-        //设置自适应屏幕，两者合用
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
-        webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        webView.setFocusable(true);// 去掉超链接的外边框
-        webView.getSettings().setDefaultTextEncodingName("GBK");//设置文本编码（根据页面要求设置： utf-8）
+//        webView.setVerticalScrollBarEnabled(false);// 取消Vertical ScrollBar显示
+//        webView.setHorizontalScrollBarEnabled(false);// 取消Horizontal ScrollBar显示
         webView.setWebChromeClient(new MyWebChromeClient());
-
-        webView.setWebViewClient(new WebViewClient() {//拦截url让接口跳转只在本app内部跳转，不跳转浏览器
+        webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
@@ -148,18 +201,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 super.onPageFinished(view, url);
             }
 
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                String url = request.getUrl().toString();
+            public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest webResourceRequest) {
+                String url = webResourceRequest.getUrl().toString();
                 if (url.startsWith("http") || url.startsWith("https")) {
-                    WebView.HitTestResult hitTestResult = view.getHitTestResult();
+                    WebView.HitTestResult hitTestResult = webView.getHitTestResult();
                     //hitTestResult==null解决重定向问题
                     if (!TextUtils.isEmpty(url) && hitTestResult == null) {
-                        view.loadUrl(url);
+                        webView.loadUrl(url);
                         return true;
                     } else {
-                        return super.shouldOverrideUrlLoading(view, url);
+                        return super.shouldOverrideUrlLoading(webView, url);
                     }
                 } else {
                     try {
@@ -167,13 +219,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         startActivity(intent);
                         return true;
                     } catch (Exception e) {
-                        Toast.makeText(MainActivity.this, "不支持的协议类型", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(MainActivity.this, "不支持的协议类型", Toast.LENGTH_SHORT).show();
                         return true;
                     }
 
                 }
-
-
             }
 
         });
@@ -184,6 +234,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (event.getAction() == KeyEvent.ACTION_DOWN) {
                     if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
                         webView.goBack();
+                        webView.removeAllViews();
                         return true;
                     }
                 }
@@ -191,20 +242,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        webView.setDownloadListener(new DownloadListener() {//下载监听，跳转到系统浏览器去下载
+        webView.setDownloadListener(new DownloadListener() {
             @Override
-            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+            public void onDownloadStart(String s, String s1, String s2, String s3, long l) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                Uri uri = Uri.parse(url);
+                Uri uri = Uri.parse(s);
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent.setData(uri);
                 startActivity(intent);
-
             }
         });
 
     }
-
 
     /**
      * 横竖屏切换监听
@@ -278,6 +327,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//竖屏
         }
+
     }
 
     @Override
@@ -289,6 +339,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             if (webview.canGoBack()) {
                 webview.goBack();
+                webview.removeAllViews();
                 return true;
             }
 
@@ -318,8 +369,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 ((ViewGroup) parent).removeView(webview);
             }
             webview.stopLoading();
-            // 退出时调用此方法，移除绑定的服务，否则某些特定系统会报错
-            webview.getSettings().setJavaScriptEnabled(false);
             webview.clearHistory();
             webview.clearView();
             webview.removeAllViews();
@@ -328,6 +377,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         super.onDestroy();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == QRSCAN_REQ_CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                qrScan();
+            } else {
+                Toast.makeText(this, "扫码需要获取相机权限，请授予", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
 
 
